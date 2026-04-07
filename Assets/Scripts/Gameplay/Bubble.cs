9b0b23ff59e022d991379b1040f51ed01f0e662d
박민재 <wer9384@naver.com>
2026-04-07 17:20:54 +0900

    feat: add BubbleColor enum and Bubble script

---
+++ Assets/Scripts/Gameplay/Bubble.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class Bubble : MonoBehaviour
+{
+    public BubbleColor Color { get; private set; }
+    public int Row { get; set; }
+    public int Col { get; set; }
+
+    private SpriteRenderer spriteRenderer;
+
+    private static readonly Color[] ColorMap = new Color[]
+    {
+        Color.red,
+        Color.blue,
+        Color.green,
+        Color.yellow,
+        new Color(0.6f, 0.2f, 0.8f), // Purple
+    };
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        var col = GetComponent<CircleCollider2D>();
+        col.radius = 0.48f;
+    }
+
+    public void SetColor(BubbleColor color)
+    {
+        Color = color;
+        if (spriteRenderer == null)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = ColorMap[(int)color];
+    }
+}
